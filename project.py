a868702c6751d8c4fca43cd923fb2e1b7263f54b
anandbs4434 <anandbs4434@gmail.com>
2023-05-10 15:41:21 +0530

    analysis

--- project.py
+++ project.py
@@ -1,10 +1,35 @@
 from flask import Flask, render_template
+import pandas as pd
+import  plotly.express as px
+import numpy as np
+
 app = Flask(__name__)
 
+def load_data():
+    df = pd.read_csv('dataset/data.csv')
+    return df 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('graph1.html')
+    
+@app.route('/graph/1')
+def graph1():
+    grouped_obj=  load_data()
+    fig = px.area(grouped_obj, x=grouped_obj.index, y='ANNUAL', title='Rainfall year-wise')
+    conclusion = 'The graph shows the yearly rainfall.'
+    fig.add_annotation(text=conclusion, xref='paper', yref='paper', x=.5, y=1, bgcolor='grey', showarrow=False, font=dict(size=12))
+  
+  
+    fig1 = px.bar(grouped_obj, x=grouped_obj.index, y='ANNUAL', title='Rainfall year-wise')
+    conclusion = 'The graph shows the yearly rainfall.'
+    fig1.add_annotation(text=conclusion, xref='paper', yref='paper', x=.5, y=1, bgcolor='grey', showarrow=False, font=dict(size=12))
+
+    return render_template('graph1.html', fig1 =fig.to_html(), fig2=fig1.to_html())
+
+
+
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8000, debug=True)
