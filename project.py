2c48d0246048d6068fce0ecf7770e7f1a0bf72ac
anandbs4434 <anandbs4434@gmail.com>
2023-05-10 18:45:52 +0530

    main

--- project.py
+++ project.py
@@ -20,12 +20,16 @@ def graph1():
     conclusion = 'The graph shows the yearly rainfall.'
     fig.add_annotation(text=conclusion, xref='paper', yref='paper', x=.5, y=1, bgcolor='grey', showarrow=False, font=dict(size=12))
   
-  
+
     fig1 = px.bar(grouped_obj, x=grouped_obj.index, y='ANNUAL', title='Rainfall year-wise')
     conclusion = 'The graph shows the yearly rainfall.'
     fig1.add_annotation(text=conclusion, xref='paper', yref='paper', x=.5, y=1, bgcolor='grey', showarrow=False, font=dict(size=12))
 
-    return render_template('graph1.html', fig1 =fig.to_html(), fig2=fig1.to_html())
+    fig2 = px.scatter(grouped_obj, x=grouped_obj.index, y='ANNUAL', title='Rainfall year-wise')
+    conclusion = 'The graph shows the yearly rainfall.'
+    fig2.add_annotation(text=conclusion, xref='paper', yref='paper', x=.5, y=1, bgcolor='grey', showarrow=False, font=dict(size=12))
+
+    return render_template('graph1.html', fig =fig.to_html(), fig1=fig1.to_html(), fig2=fig2.to_html())
 
 
 
