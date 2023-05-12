a8a45934589fc2ca374e8a002c320719da74a659
anandbs4434 <anandbs4434@gmail.com>
2023-05-12 21:54:30 +0530

    visvualisation

--- project.py
+++ project.py
@@ -11,11 +11,12 @@ def load_data():
 
 @app.route('/')
 def index():
-    return render_template('graph2.html')
+    return render_template('graph12.html')
     
 @app.route('/graph/1')
 def graph1():
-    grouped_obj=  load_data()
+    rain=  load_data()
+    grouped_obj = rain.groupby(["YEAR"]).sum()
     fig = px.area(grouped_obj, x=grouped_obj.index, y='ANNUAL', title='Rainfall year-wise')
     conclusion = 'The graph shows the yearly rainfall.'
     fig.add_annotation(text=conclusion, xref='paper', yref='paper', x=.5, y=1, bgcolor='grey', showarrow=False, font=dict(size=12))
@@ -34,7 +35,8 @@ def graph1():
 
 @app.route('/graph/2')
 def graph2():
-    grouped_obj=  load_data()
+    rain=  load_data()
+    grouped_obj = rain.groupby(["YEAR"]).sum()
     month = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN']
     fig = px.bar(grouped_obj, x=grouped_obj.index, y=month, title='Rainfall semi-annual',labels={'value':'Rainfall in mm'})
     conclusion = 'The graph shows the Half Yearly rainfall.'
@@ -57,7 +59,8 @@ def graph2():
 
 @app.route('/graph/3')
 def graph3():
-    grouped_obj=  load_data()
+    rain=  load_data()
+    grouped_obj = rain.groupby(["YEAR"]).sum()
     month = ['APR', 'MAY', 'JUN']
     fig = px.bar(grouped_obj, x=grouped_obj.index, y=month, title='Rainfall quater-wise',labels={'value':'Rainfall in mm'})
     conclusion = 'The graph shows the monthly rainfall.'
@@ -78,7 +81,8 @@ def graph3():
 
 @app.route('/graph/4')
 def graph4():
-    grouped_obj=  load_data()
+    rain=  load_data()
+    grouped_obj = rain.groupby(["YEAR"]).sum()
     fig = px.scatter(grouped_obj, x=grouped_obj.index, y='ANNUAL', title='Rainfall year-wise')
     conclusion = 'The graph shows the yearly rainfall.'
     fig.add_annotation(text=conclusion, xref='paper', yref='paper', x=.5, y=1, bgcolor='grey', showarrow=False, font=dict(size=12))
@@ -99,7 +103,8 @@ def graph4():
 @app.route('/graph/5')
 def graph5():
     rain=  load_data()
-    grouped_obj= load_data()
+    grouped_obj = rain.groupby(["SUBDIVISION"]).sum()
+    grouped_obj.drop(['YEAR'], axis=1, inplace=True)
     fig = px.area(rain, x='SUBDIVISION', y=['ANNUAL'], title='Average Rainfall ',labels={'value':'Rainfall in mm'})
     mean_rainfall = rain['ANNUAL'].mean()
     fig.add_annotation(xref='paper', yref='paper', x=0.1, y=1, text=f"Average Rain = {mean_rainfall:.0f}",showarrow=False)
@@ -119,9 +124,12 @@ def graph5():
     return render_template('graph5.html', fig =fig.to_html(), fig1=fig1.to_html(), fig2=fig2.to_html())
 
 
+
 @app.route('/graph/6')
 def graph6():
-    grouped_obj=  load_data()
+    rain=  load_data()
+    grouped_obj = rain.groupby(["SUBDIVISION"]).sum()
+    grouped_obj.drop(['YEAR'], axis=1, inplace=True)
     month = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN']
     fig = px.funnel(grouped_obj, x=grouped_obj.index, y=month, title='State-wise',height=600)
     conclusion = 'The graph shows rainfall from every State and UT.'
@@ -144,7 +152,9 @@ def graph6():
 
 @app.route('/graph/7')
 def graph7():
-    grouped_obj=  load_data()
+    rain=  load_data()
+    grouped_obj = rain.groupby(["SUBDIVISION"]).sum()
+    grouped_obj.drop(['YEAR'], axis=1, inplace=True)
     month = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN']
     fig = px.ecdf(grouped_obj,x=grouped_obj.index, y=month, title='State-wise',height=600)
     conclusion = 'The graph shows rainfall from every State and UT.'
@@ -168,7 +178,9 @@ def graph7():
 
 @app.route('/graph/8')
 def graph8():
-    grouped_obj=  load_data()
+    rain=  load_data()
+    grouped_obj = rain.groupby(["SUBDIVISION"]).sum()
+    grouped_obj.drop(['YEAR'], axis=1, inplace=True)
     month = ['APR', 'MAY', 'JUN']
     fig = px.line(grouped_obj, x=grouped_obj.index, y=month, title='State-wise',height=600,labels={'value':'Rainfall in mm'})
     conclusion = 'The graph shows rainfall from every State and UT.'
@@ -191,25 +203,38 @@ def graph8():
 
 @app.route('/graph/9')
 def graph9():
-    grouped_obj =  load_data()
+    rain =  load_data()
+    grouped_obj = rain.groupby(["YEAR"]).sum()
+    grouped_obj.drop(['ANNUAL'], axis=1, inplace=True)
     month = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT','NOV', 'DEC']
     fig = px.area(grouped_obj, x=grouped_obj.index, y=month, title='Rainfall YEAR-wise',labels={'value':'Rainfall in mm'})
     conclusion = 'The graph shows  monthly rainfall.'
     fig.add_annotation(text=conclusion, xref='paper', yref='paper', x=.5, y=1, bgcolor='grey', showarrow=False, font=dict(size=12))
     fig.show()
 
+
+    return render_template('graph9.html', fig =fig.to_html())
+
+@app.route('/graph/10')
+def graph10():
+    rain=  load_data()
+    grouped_obj = rain.groupby(['ANNUAL']).sum()
+    grouped_obj.drop(['YEAR'], axis=1, inplace=True)
+    grouped_obj.reset_index(inplace=True)
     fig1 = px.scatter(grouped_obj, x='ANNUAL', y=['JAN','FEB','MAR'], title='Rainfall in First Quarter', trendline='ols', log_y=True,labels={'value':'Rainfall in mm'})
     fig1.show()
 
     fig2 = px.scatter(grouped_obj, x='ANNUAL', y=['APR','MAY','JUN'], title='Rainfall in Second Quarter', trendline='ols', log_y=True,labels={'value':'Rainfall in mm'})
     fig2.show()
 
-    return render_template('graph9.html', fig =fig.to_html(), fig1=fig1.to_html(), fig2=fig2.to_html())
+    return render_template('graph10.html', fig1 =fig1.to_html(), fig2=fig2.to_html())
 
-
-@app.route('/graph/10')
-def graph10():
-    grouped_obj =  load_data()
+@app.route('/graph/11')
+def graph11():
+    rain =  load_data()
+    grouped_obj = rain.groupby(['ANNUAL']).sum()
+    grouped_obj.drop(['YEAR'], axis=1, inplace=True)
+    grouped_obj.reset_index(inplace=True)
     fig = px.scatter(grouped_obj, x='ANNUAL', y=['JUL','AUG','SEP'], title='Rainfall in Third Quarter', trendline='ols', log_y=True,labels={'value':'Rainfall in mm'})
     fig.show()
 
@@ -221,14 +246,30 @@ def graph10():
 
     return render_template('graph10.html', fig =fig.to_html(), fig1=fig1.to_html(), fig2=fig2.to_html())
 
-@app.route('/graph/11')
-def graph11():
-    grouped_obj =  load_data()
+@app.route('/graph/12')
+def graph12():
+    rain =  load_data()
+    grouped_obj = rain.groupby(['ANNUAL']).sum()
+    grouped_obj.drop(['YEAR'], axis=1, inplace=True)
+    grouped_obj.reset_index(inplace=True)
     fig = px.scatter(grouped_obj, x=['FEB','APR','JUN', 'AUG', 'OCT', 'DEC'], y='ANNUAL', title='Rainfall in Half-Yearly Basis', trendline='ols', log_y=True,labels={'value':'Rainfall in mm'})
     fig.show()
 
-    return render_template('graph11.html', fig =fig.to_html())  
+    return render_template('graph12.html', fig =fig.to_html())  
 
+
+@app.route('/graph/13')
+def graph13():
+    rain=  load_data()
+    rain.sort_values(by="YEAR", inplace=True)
+    fig=px.scatter_mapbox(
+         rain, lat="Latitude", lon="Longitude", color="ANNUAL", size="ANNUAL",
+    color_continuous_scale=px.colors.sequential.Plasma, size_max=50, zoom=3,
+    mapbox_style="carto-positron", title='Annual Rainfall in India', height=800,
+    animation_frame='YEAR', animation_group='SUBDIVISION')
+    fig.show()
+
+    return render_template('graph13.html', fig =fig.to_html())
 
 def retreive_data():
     df = pd.read_csv('dataset/rainfall_India_2017.csv')
